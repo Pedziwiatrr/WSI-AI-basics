433cac61d7ab2e624ebdda862d40f3b9b0886521
Pedziwiatrr <michalozaurus@gmail.com>
2024-10-20 22:21:55 +0200

    formatting changes

--- Zadanie1/gradient.py
+++ Zadanie1/gradient.py
@@ -1,5 +1,7 @@
-# Nr indeksu: 331421
-# A = 1, B = 2, C = 4
-
+# Nr indeksu: 331421 => A = 1, B = 2, C = 4
 # 1: f(x) = x + 2sin(x),    D = (-4pi, 4pi)
-# 2: g(x, y) = 4xy/e^(x^2 + y^2)
+# 2: g(x, y) = 4xy/e^(x^2 + y^2),   Dx = (-2, 2), Dy  (-2, 2)
+
+
+def grad_descent(function):
+    pass
