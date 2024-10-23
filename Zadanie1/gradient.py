477e77fb795f6bbda92ee85d2def6244f520c0a0
Pedziwiatrr <michalozaurus@gmail.com>
2024-10-23 22:50:59 +0200

    included domains in grad_descent

--- Zadanie1/gradient.py
+++ Zadanie1/gradient.py
@@ -42,11 +42,18 @@ def grad_descent(derivative, learning_rate, args, step_count=100, find_min=True)
     if find_min:
         learning_rate = -learning_rate
         # domyślnie dążymy do minimum. Jeśli find_min = False, funkcja będzie dążyć do maksimum
+
     for i in range(step_count):
-        new_args = [
-            variable + learning_rate * derivative(*args)[i]
-            for i, variable in enumerate(args)
-        ]
+        try:
+            new_args = [
+                variable + learning_rate * derivative(*args)[i]
+                for i, variable in enumerate(args)
+            ]
+            # args - argumenty funkcji na których obecnie "jesteśmy"
+            # każdy z tych argumentów zmieniamy z osobna co każdy krok gradientu przesuwając się po wykresie
+        except TypeError:
+            break
+            # TypeError oznacza wyjście poza dziedzinę (None jako wartość funkcji derivative)
         args = new_args
 
     return args
