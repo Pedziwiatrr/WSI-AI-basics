12fbc0e9244da91a629bfa29bd6fd87925c6d114
Pedziwiatrr <michalozaurus@gmail.com>
2024-10-23 22:05:06 +0200

    added derivatives functions and finished grad_descent function

--- Zadanie1/gradient.py
+++ Zadanie1/gradient.py
@@ -13,6 +13,13 @@ def f(x):
         return None
 
 
+def f_derivative(x):
+    if -4 * math.pi < x < 4 * math.pi:
+        return 1 + 2 * math.cos(x)
+    else:
+        return None
+
+
 def g(x, y):
     if -2 < x < 2 and -2 < y < 2:
         return 4 * x * y / math.e ** (x**2 + y**2)
@@ -20,9 +27,30 @@ def g(x, y):
         return None
 
 
-def grad_descent(function, learning_rate, position, step_count=100, find_min=True):
+def g_derivative(x, y):
+    if -2 < x < 2 and -2 < y < 2:
+        return (
+            (4 - 8 * x**2) * y * math.e ** ((-x) ** 2 - (y) ** 2),
+            (4 - 8 * y**2) * x * math.e ** ((-x) ** 2 - (y) ** 2),
+        )
+        # pochodne cząstkowe (∂x, ∂y)
+    else:
+        return None
+
+
+def grad_descent(derivative, learning_rate, position, step_count=100, find_min=True):
     if find_min:
         learning_rate = -learning_rate
+        # domyślnie dążymy do minimum. Jeśli find_min = False, funkcja będzie dążyć do maksimum
     for i in range(step_count):
-        new_position = position + learning_rate  # * pochodna
+        new_position = [
+            variable + learning_rate * derivative(*position) for variable in position
+        ]
         position = new_position
+
+    return position
+
+
+if __name__ == "__main__":
+    position = grad_descent(f_derivative, 0.01, [f(random.uniform(-2, 2))], 1000)
+    print(position)
