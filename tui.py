ac53300febaae49a28d22bec1073f1c6e21149b9
Pedziwiatrr <michalozaurus@gmail.com>
2024-11-27 23:47:33 +0100

    Changed moves coordinates order to fix one of the problems

--- tui.py
+++ tui.py
@@ -13,15 +13,13 @@ class GameTUI:
             if self.game.get_winner() != "":
                 print(f"THE WINNER IS: {self.game.get_winner()}!!!")
                 break
-            else:
-                print(self.game.get_winner())
             current_player = self.player_x if self.game.player_x_turn else self.player_o
             print(f"It is {current_player.char}'s turn!")
             if current_player.is_human:
-                column, row = self.get_human_move()
+                row, column = self.get_human_move()
             else:
-                column, row = self.get_ai_move()
-            self.place_char(column, row, current_player.char)
+                row, column = self.get_ai_move()
+            self.place_char(row, column, current_player.char)
             self.game.player_x_turn = not self.game.player_x_turn
 
     def draw_row(self, row):
@@ -35,7 +33,6 @@ class GameTUI:
         print(row_str)
 
     def draw_board(self, board):
-        print()
         for row in board:
             self.draw_row(row)
         print()
@@ -44,12 +41,12 @@ class GameTUI:
         print("Your turn!")
         while True:
             try:
-                column = int(input("Enter column (1-3): "))
                 row = int(input("Enter row (1-3): "))
+                column = int(input("Enter column (1-3): "))
                 break
             except ValueError:
                 print("Invalid input. Enter numbers between 1 and 3")
-        return column -1, row-1
+        return row-1, column -1
 
     def get_ai_move(self):
         if self.game.player_x_turn:
@@ -57,7 +54,7 @@ class GameTUI:
         else:
             return self.player_o.get_move()
 
-    def place_char(self, column, row, char):
+    def place_char(self, row, column, char):
         self.game.board[row, column] = char
 
 
