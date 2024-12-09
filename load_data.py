6bff3e5d5a3471e7d6ace30b51ae4eda88ee4ea3
Pedziwiatrr <michalozaurus@gmail.com>
2024-12-09 20:58:56 +0100

    changed replacing mechanism and column args

--- load_data.py
+++ load_data.py
@@ -3,12 +3,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def get_data(chosen_columns: list):
+def get_data(excluded_columns: list):
     # fetch dataset
     breast_cancer_wisconsin_diagnostic = fetch_ucirepo(id=17)
 
     # data (as pandas dataframes)
     X = breast_cancer_wisconsin_diagnostic.data.features
+    chosen_columns = [col for col in X.columns if col not in excluded_columns]
     y = breast_cancer_wisconsin_diagnostic.data.targets
 
     data = X.copy()
@@ -18,15 +19,11 @@ def get_data(chosen_columns: list):
     y = data['diagnosis']
 
     # convert string values to binary for our algorithm
-    for i in range(len(y)):
-        if y[i] == 'M':
-            y[i] = 1
-        elif y[i] == 'B':
-            y[i] = 0
+    y = y.replace({'M': 1, 'B': 0})
 
     return X, y
 
 
-def prepare_data(test_ratio: float=0.25):
+def prepare_data(X, y, test_ratio: float=0.25):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio, random_state=69)
     return X_train, X_test, y_train, y_test
