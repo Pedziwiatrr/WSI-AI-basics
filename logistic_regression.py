ef93e4a1ddde6c1bfe04498bf31f043a41707e21
Pedziwiatrr <michalozaurus@gmail.com>
2024-12-10 03:10:24 +0100

    implemented minimize_weights function with grad descent to minimize cost by adjusting weights values. Added documentation

--- logistic_regression.py
+++ logistic_regression.py
@@ -6,29 +6,45 @@ def sigmoid(value):
     return 1 / ( 1 + np.exp(-value))
 
 
-def calculate_cost(X, y, weights):
+def predict_value(X, weights):
+    weighted_sum = 0
+    for i in range(len(weights)):
+        # predict y value based on all features (X) values and their weights
+        weighted_sum += X[i] * weights[i]
+    # convert predicted value to probability
+    predicted_value = sigmoid(weighted_sum)
+    return predicted_value
+
+
+def evaluate_total_cost(X, y, weights):
+    """ calculate total cost of all values in data """
     total_cost = 0
     for i in range(len(y)):
-        weighted_sum = 0
-        feature_row = X[i]
-        for j in range(len(weights)):
-            weighted_sum += feature_row[j] * weights[j]
-        predict_value = sigmoid(weighted_sum)
+        predicted_value = predict_value(X[i], weights)
+        # if the prediction was wrong, raise cost value (logx < 0 for x < 1 )
         if y[i] == 1:
-            total_cost += -np.log(predict_value)
+            total_cost -= np.log(predicted_value)
         else:
-            total_cost += -np.log(1 - predict_value)
+            total_cost -= np.log(1 - predicted_value)
     total_cost /= len(y)
     return total_cost
 
-def update_weights():
-    pass
 
-def train():
-    pass
+def adjust_weights(X, y, weights, step_length):
+    """ adjust weights values using gradient descent algorithm to minimize cost """
+    # gradient multipliers for every features weight
+    weights_gradients = [0] * len(weights)
+    for i in range(len(y)):
+        predicted_value = predict_value(X[i], weights)
+        error = predicted_value - y[i]
+        for j in range(len(weights)):
+            # error serves a role of a derivative. Bigger error -> needs a bigger value change
+            weights_gradients[j] += error * X[i][j]
+    for i in range(len(weights)):
+        # Wi = Wi - 1/n * sum(predicted_value(xi) - y)xi
+        weights[i] -= step_length * weights_gradients[i] / len(y)
+    return weights
 
-def predict_values():
-    pass
 
 
 
