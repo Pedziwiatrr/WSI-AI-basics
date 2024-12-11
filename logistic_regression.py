4d819885466c7d20e6cf480d84e7ab54315bc5c4
Pedziwiatrr <michalozaurus@gmail.com>
2024-12-11 05:43:45 +0100

    Fixed predict function it seems to be working but the values are yet to be tested

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,16 +1,21 @@
 import numpy as np
 
 
+def round_to_binary(value):
+    if value >= 0.5:
+        return 1
+    else:
+        return 0
+
 def sigmoid(value):
     # converts any value to probability (in range 0 to 1)
     return 1 / ( 1 + np.exp(-value))
-
 
 def predict_value(X, weights):
     weighted_sum = 0
     for i in range(len(weights)):
         # predict y value based on all features (X) values and their weights
-        weighted_sum += X[i] * weights[i]
+        weighted_sum += X.iloc[i] * weights[i]
     # convert predicted value to probability
     predicted_value = sigmoid(weighted_sum)
 
@@ -20,7 +25,6 @@ def predict_value(X, weights):
         predicted_value = 1 - 1e-6
     #print(predicted_value)
     return predicted_value
-
 
 def evaluate_total_cost(X, y, weights):
     """ calculate total cost of all values in data """
@@ -35,7 +39,6 @@ def evaluate_total_cost(X, y, weights):
     total_cost /= len(y)
     #print(total_cost)
     return total_cost
-
 
 def adjust_weights(X, y, weights, step_length=0.01):
     """ adjust weights values using gradient descent algorithm to minimize cost """
@@ -52,12 +55,13 @@ def adjust_weights(X, y, weights, step_length=0.01):
         weights[i] -= step_length * weights_gradients[i] / len(y)
     return weights
 
-
 def learn(data, iterations, step_length=None):
+    """ adjust weights by minimizing the cost (diagnosis error) """
     [X_train, X_test, y_train, y_test] = data
     weights = []
     for feature in X_train:
         weights.append(1)
+    print (f"len(weights): {len(weights)}")
     for i in range(iterations):
         weights = adjust_weights(X_train, y_train, weights, step_length)
         if i % 100 == 0:
@@ -65,7 +69,23 @@ def learn(data, iterations, step_length=None):
             print(f"Weights: {weights}\n")
 
     final_cost = evaluate_total_cost(X_train, y_train, weights)
-    print(f"Final cost: {final_cost}")
+    print(f"Final learning cost: {final_cost}")
+    return weights
+
+def predict(data, weights):
+    """ use weights from learn function to diagnose cases from the rest of the data"""
+    [X_train, X_test, y_train, y_test] = data
+    diagnosis_list = []
+    for id, case in X_test.iterrows():
+        #print(f"Current case: {case}")
+        prediction = predict_value(case, weights)
+        diagnosis = round_to_binary(prediction)
+        diagnosis_list.append(diagnosis)
+    print(f"Diagnosis_list: {diagnosis_list}")
+    return diagnosis_list
 
 def logistic_regression(data, iterations, step_length=None):
-    learn(data, iterations, step_length)
+    weights = learn(data, iterations, step_length)
+    diagnosis_list = predict(data, weights)
+    return diagnosis_list
+
