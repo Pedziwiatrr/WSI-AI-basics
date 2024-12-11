4bb345ef5b0ae483a832b698b8f1bf450ce39f91
Pedziwiatrr <michalozaurus@gmail.com>
2024-12-11 08:12:54 +0100

    Removed evaluate_total_cost function beacuse after concept change i realised its kinda useless

--- logistic_regression.py
+++ logistic_regression.py
@@ -26,34 +26,22 @@ def predict_value(X, weights):
     #print(predicted_value)
     return predicted_value
 
-def evaluate_total_cost(X, y, weights):
-    """ calculate total cost of all values in data """
-    total_cost = 0
-    for i in range(len(y)):
-        predicted_value = predict_value(X.iloc[i], weights)
-        # if the prediction was wrong, raise cost value (logx < 0 for x < 1 )
-        if y.iloc[i] == 1:
-            total_cost -= np.log(predicted_value)
-        else:
-            total_cost -= np.log(1 - predicted_value)
-    total_cost /= len(y)
-    #print(total_cost)
-    return total_cost
-
 def adjust_weights(X, y, weights, step_length=0.01):
     """ adjust weights values using gradient descent algorithm to minimize cost """
     # gradient multipliers for every features weight
     weights_gradients = [0] * len(weights)
+    total_error = 0
     for i in range(len(y)):
         predicted_value = predict_value(X.iloc[i], weights)
         error = predicted_value - y.iloc[i]
+        total_error += error
         for j in range(len(weights)):
             # error serves a role of a derivative. Bigger error -> needs a bigger value change
             weights_gradients[j] += error * X.iloc[i, j]
     for i in range(len(weights)):
         # Wi = Wi - 1/n * sum(predicted_value(xi) - y)xi
         weights[i] -= step_length * weights_gradients[i] / len(y)
-    return weights
+    return weights, total_error
 
 def learn(data, iterations, step_length=None):
     """ adjust weights by minimizing the cost (diagnosis error) """
@@ -61,15 +49,12 @@ def learn(data, iterations, step_length=None):
     weights = []
     for feature in X_train:
         weights.append(1)
-    print (f"len(weights): {len(weights)}")
+    #print (f"len(weights): {len(weights)}")
     for i in range(iterations):
-        weights = adjust_weights(X_train, y_train, weights, step_length)
-        if i % 100 == 0:
-            print(f"Iteration: {i}, Current cost: {evaluate_total_cost(X_train, y_train, weights)}")
+        weights, error = adjust_weights(X_train, y_train, weights, step_length)
+        if i % 1 == 0:
+            print(f"Iteration: {i}, Error: {error}")
             print(f"Weights: {weights}\n")
-
-    final_cost = evaluate_total_cost(X_train, y_train, weights)
-    print(f"Final learning cost: {final_cost}")
     return weights
 
 def predict(data, weights):
@@ -81,7 +66,7 @@ def predict(data, weights):
         prediction = predict_value(case, weights)
         diagnosis = round_to_binary(prediction)
         diagnosis_list.append(diagnosis)
-    print(f"Diagnosis_list: {diagnosis_list}")
+    #print(f"Diagnosis_list: {diagnosis_list}")
     return diagnosis_list
 
 def logistic_regression(data, iterations, step_length=None):
