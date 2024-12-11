abdab27253446c1359aceb3425fe227fc8764237
Pedziwiatrr <michalozaurus@gmail.com>
2024-12-11 08:52:13 +0100

    Changed code structure, changed and moved functions to interpret_results.py

--- main.py
+++ main.py
@@ -1,38 +1,12 @@
 import argparse
 from load_data import get_data, prepare_data
 from logistic_regression import logistic_regression
+from interpret_results import print_results, interpret_results
 
-
-def compare_diagnoses(first_diagnosis, second_diagnosis):
-    same_verdicts = 0
-    different_verdicts = 0
-    for i in range(len(first_diagnosis)):
-        verdict1 = first_diagnosis.iloc[i]
-        verdict2 = second_diagnosis[i]
-        print(f"{verdict1} vs {verdict2}")
-        if verdict1 == verdict2:
-            same_verdicts += 1
-        else:
-            different_verdicts += 1
-    return [same_verdicts, different_verdicts]
-
-
-def print_results(verdicts):
-    print("=" * 100)
-    total_verdicts = verdicts[0] + verdicts[1]
-    print(f"Same verdicts: {verdicts[0]}")
-    print(f"Different verdicts: {verdicts[1]}")
-    accuracy = verdicts[0] / total_verdicts
-    print(f"Algorithm accuracy: {accuracy*100:.2f}%")
-    if accuracy > 0.9:
-        print(":)")
-    else:
-        print(":(")
-    print("="*100)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--iterations", type=int, default=5000)
+    parser.add_argument("--iterations", type=int, default=300)
     parser.add_argument("--step_length", type=float, default=0.001)
     parser.add_argument("--excluded_columns", type=str, default="")
     args = parser.parse_args()
@@ -42,11 +16,11 @@ def main():
 
     X, y = get_data(excluded_columns)
     data = prepare_data(X, y)
-    test_diagnosis = logistic_regression(data, args.iterations, args.step_length)
+    test_diagnosis, predicted_probabilities = logistic_regression(data, args.iterations, args.step_length)
     #print(f"Real diagnosis: {data[3].values.tolist()}")
     #print(f"Algorithm diagnosis: {test_diagnosis}")
-    test_results = compare_diagnoses(data[3], test_diagnosis)
-    print_results(test_results)
+    result_metrics = interpret_results(data[3], test_diagnosis, predicted_probabilities)
+    print_results(*result_metrics)
 
 
 if __name__ == "__main__":
