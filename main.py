baf74505537264d9eeb7bc551d1a562e104726ca
Wojtek Zieziula <wojciech.zieziula@gmail.com>
2024-12-29 22:15:22 +0100

    minor changes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from load_data import get_data, prepare_data
 def main():
     X, y = get_data()
     data = prepare_data(X, y, test_ratio=0.25, seed=69)
+    X_train, X_test, y_train, y_test = data
 
 
 if __name__ == '__main__':
