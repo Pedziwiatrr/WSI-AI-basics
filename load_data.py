09826f4a28749058348aa430dda349a23bad203e
Wojtek Zieziula <wojciech.zieziula@gmail.com>
2024-12-29 22:12:58 +0100

    added data scaling and mean filling

--- load_data.py
+++ load_data.py
@@ -1,4 +1,5 @@
 from ucimlrepo import fetch_ucirepo
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
 
@@ -10,15 +11,18 @@ def get_data():
     X = wine_quality.data.features
     y = wine_quality.data.targets
 
-    # metadata
     print(wine_quality.metadata)
-
-    # variable information
     print(wine_quality.variables)
 
     return X, y
 
 
 def prepare_data(X, y, test_ratio: float=0.25, seed=69):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio, random_state=seed)
+    print(X.isnull().sum())
+    X = X.fillna(X.mean())  # if any value is missing its replaced by mean of other values of this feature
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    # scaling data
+    # mean = 0, variance = 1
+    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=test_ratio, random_state=seed)
     return [X_train, X_test, y_train, y_test]
