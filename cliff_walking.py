537df055ea1dfdd4be670c7bfb4ed7ee06fd1c1a
Pedziwiatrr <michalozaurus@gmail.com>
2025-01-15 18:59:01 +0100

    minor changes to match gymnasium.farma.org convention

--- cliff_walking.py
+++ cliff_walking.py
@@ -3,21 +3,21 @@ import numpy as np
 
 def q_learn(environment, episode_count=10, max_steps=1000, beta=0.8, gamma=0.1, epsilon=0.2):
     episode = 0
-    # Q will store possible rewards for every action in every state. We dont know the values yet so right now its just np.zeros
+    # Q will store possible rewards for every action in every state. We don't know the values yet so right now its just np.zeros
     Q = np.zeros((environment.observation_space.n, environment.action_space.n))                 # Q ← init
     while episode < episode_count:
         sum_reward = 0
-        state, info = environment.reset()
+        state, info = environment.reset(return_info=True)
         steps = 0                                                                                   # t ← 0
         while steps < max_steps:
             action = choose_action(Q, state, epsilon, environment.action_space.n)                   # at ← choose action (xt,Qt)
-            next_state, reward, is_finished, info1, info2 =  environment.step(action)               # rt, xt+1 ← do at
+            next_state, reward, terminated, truncated, info = environment.step(action)              # rt, xt+1 ← do at
 
             # delta will store the error between our current state and the next state
             # predicted future reward is discounted by gamma because
             # we don't want it to impact our decisions too much as it might be incorrect.
             delta = reward + gamma * np.max(Q[next_state, :]) - Q[state, action]                    # ∆ ← rt + γ max Qt(xt+1,a) − Qt(xt,at)
-            Q[state, action] += beta * delta                                                         # Qt+1 ← Qt + β∆
+            Q[state, action] += beta * delta                                                        # Qt+1 ← Qt + β∆
 
             state = next_state
             sum_reward += reward
@@ -25,7 +25,7 @@ def q_learn(environment, episode_count=10, max_steps=1000, beta=0.8, gamma=0.1, 
             if steps % 100 == 0:
                 print(f' Step: {steps} | Reward sum: {sum_reward}')
 
-            if is_finished:
+            if terminated or truncated:
                 break
 
         episode += 1
