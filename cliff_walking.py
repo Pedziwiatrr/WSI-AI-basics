af14840c200ca7ce177c7916dc906df131dd9511
Pedziwiatrr <michalozaurus@gmail.com>
2025-01-15 18:48:31 +0100

    implemented choose_action, changes to q_learn, added more comments explaining what is happening

--- cliff_walking.py
+++ cliff_walking.py
@@ -1,23 +1,42 @@
-import gymnasium as gym
 import numpy as np
 
-def q_learn(environment, episode_count=10, max_steps=1000, beta=0.8, gamma=0.1):
+def q_learn(environment, episode_count=10, max_steps=1000, beta=0.8, gamma=0.1, epsilon=0.2):
     episode = 0
     while episode < episode_count:
         sum_reward = 0
         state, info = environment.reset()
         steps = 0                                                                                   # t ← 0
+        # Q will store possible rewards for every action in every state. We dont know the values yet so right now its just np.zeros
         Q = np.zeros((environment.observation_space.n, environment.action_space.n))                 # Q ← init
         while steps < max_steps:
-            action = choose_action()                                                                # at ← choose action (xt,Qt)
-            next_state, reward, is_finished, is_finished, info =  environment.step(action)          # rt, xt+1 ← do at
+            action = choose_action(Q, state, epsilon, environment.action_space.n)                   # at ← choose action (xt,Qt)
+            next_state, reward, is_finished, info1, info2 =  environment.step(action)               # rt, xt+1 ← do at
+
+            # delta will store the error between our current state and the next state
+            # predicted future reward is discounted by gamma because
+            # we don't want it to impact our decisions too much as it might be incorrect.
             delta = reward + gamma * np.max(Q[next_state, :]) - Q[state, action]                    # ∆ ← rt + γ max Qt(xt+1,a) − Qt(xt,at)
             Q[state, action] = beta * delta                                                         # Qt+1 ← Qt + β∆
+
+            state = next_state
             sum_reward += reward
             steps += 1                                                                              # t ← t + 1
+            if steps % 100 == 0:
+                print(f' Step: {steps} | Reward sum: {sum_reward}')
+
+            if is_finished:
+                break
+
         episode += 1
+        print(f'>> Finished episode: {episode}, steps: {steps}, Reward sum: {sum_reward} <<')
 
 
-def choose_action():
-    return 0
+def choose_action(Q, state, epsilon, possible_actions):
+    if np.random.rand() < epsilon:
+        # exploration: trying out new actions that are not yet explored
+        action = np.random.choice(possible_actions)
+    else:
+        # exploatation: choosing the best explored action
+        action = np.argmax(Q[state, :])
+    return action
 
