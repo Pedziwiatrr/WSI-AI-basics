d53a89a8093f8a6f2801af53ee835406fd455cad
Pedziwiatrr <michalozaurus@gmail.com>
2025-01-15 18:52:07 +0100

    Fixed 2 oversights. 1: Moved Q init before episodes loop beacuse it was resetting every episode. 2: fixed Q updates by replacing setting (=) with incrementing (+=)

--- cliff_walking.py
+++ cliff_walking.py
@@ -2,12 +2,12 @@ import numpy as np
 
 def q_learn(environment, episode_count=10, max_steps=1000, beta=0.8, gamma=0.1, epsilon=0.2):
     episode = 0
+    # Q will store possible rewards for every action in every state. We dont know the values yet so right now its just np.zeros
+    Q = np.zeros((environment.observation_space.n, environment.action_space.n))                 # Q ← init
     while episode < episode_count:
         sum_reward = 0
         state, info = environment.reset()
         steps = 0                                                                                   # t ← 0
-        # Q will store possible rewards for every action in every state. We dont know the values yet so right now its just np.zeros
-        Q = np.zeros((environment.observation_space.n, environment.action_space.n))                 # Q ← init
         while steps < max_steps:
             action = choose_action(Q, state, epsilon, environment.action_space.n)                   # at ← choose action (xt,Qt)
             next_state, reward, is_finished, info1, info2 =  environment.step(action)               # rt, xt+1 ← do at
@@ -16,7 +16,7 @@ def q_learn(environment, episode_count=10, max_steps=1000, beta=0.8, gamma=0.1, 
             # predicted future reward is discounted by gamma because
             # we don't want it to impact our decisions too much as it might be incorrect.
             delta = reward + gamma * np.max(Q[next_state, :]) - Q[state, action]                    # ∆ ← rt + γ max Qt(xt+1,a) − Qt(xt,at)
-            Q[state, action] = beta * delta                                                         # Qt+1 ← Qt + β∆
+            Q[state, action] += beta * delta                                                         # Qt+1 ← Qt + β∆
 
             state = next_state
             sum_reward += reward
