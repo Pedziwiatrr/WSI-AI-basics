3f256e43d93394533b61862b0e92f37a010c5f3c
Pedziwiatrr <michalozaurus@gmail.com>
2025-01-15 19:55:02 +0100

    added argparser, connected q_learn to modified version of linked visualization

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import argparse
 import gymnasium as gym
 from cliff_walking import q_learn
+from visualizer import postprocess, qtable_directions_map, plot_q_values_map
 
 
 def main():
-    environment = gym.make("CliffWalking-v0")
-    q_learn(environment)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--episodes', type=int, default=100)
+    parser.add_argument('--max_steps', type=int, default=1000)
+    parser.add_argument('--epsilon', type=float, default=0.2)
+    parser.add_argument('--beta', type=float, default=0.1)
+    parser.add_argument('--gamma', type=float, default=0.9)
+    args = parser.parse_args()
+
+    environment = gym.make("CliffWalking-v0", render_mode="rgb_array")
+    Q, rewards_list, steps_list = q_learn(environment, args.episodes, args.max_steps, args.beta, args.gamma, args.epsilon)
+
+    episodes = list(range(1, args.episodes + 1))
+    results, summary = postprocess(episodes, rewards_list, steps_list)
+
+    results.to_csv('qlearning_results.csv', index=False)
+
+    plot_q_values_map(Q, environment, "q_values_map.png")
 
 
 if __name__ == '__main__':
