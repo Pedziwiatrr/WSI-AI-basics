6cceebb234bb3c27ee8f61e1e4c06ce0d17335c7
Pedziwiatrr <michalozaurus@gmail.com>
2025-01-24 00:54:23 +0100

    implemented converting values to numerical for our network

--- handle_data.py
+++ handle_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from sklearn.preprocessing import LabelEncoder
 
 CSV_PATH = "us-crime-dataset/US_Crime_DataSet.csv"
 FEATURES =  [
@@ -7,10 +7,18 @@ FEATURES =  [
 ]
 
 
-def prepare_data(csv_file=CSV_PATH, selected_features=FEATURES):
-    data = pd.read_csv(csv_file)
+def get_prepared_data(csv_file=CSV_PATH, selected_features=FEATURES):
+    data = pd.read_csv(csv_file, low_memory=False)
     data = data[selected_features]
     prepared_data = data.dropna()
 
     return prepared_data
 
+
+def get_numerical_data(prepared_data=get_prepared_data()):
+    encoded_data = prepared_data.copy()
+    for column in prepared_data.columns:
+        encoder = LabelEncoder()
+        encoded_data[column] = encoder.fit_transform(prepared_data[column])
+
+    return encoded_data
