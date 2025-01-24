c70596da2b0f7b9fb96e42a71e5392cca705d28f
Pedziwiatrr <michalozaurus@gmail.com>
2025-01-24 01:22:17 +0100

    updated main after implementing network initialization

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import argparse
+from handle_data import get_numerical_data
+from bayesian_network import create_network
+from visualizer import visualize_network
 
 
 def main():
-    pass
+    data = get_numerical_data()
+    network = create_network(data)
+    visualize_network(network)
 
 if __name__ == '__main__':
     main()
