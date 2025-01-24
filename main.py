af17ae29b3e9f029ff0f6987240e3b0968ca971d
Pedziwiatrr <michalozaurus@gmail.com>
2025-01-24 05:33:33 +0100

    added generator usage

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ import matplotlib
 from handle_data import get_numerical_data, get_prepared_data
 from bayesian_network import create_network, get_feature_probability_distribution, get_feature_dependencies, get_marginal_probabilities
 from visualizer import visualize_network, print_feature_dependencies, print_probabilities_distribution, plot_probability_distributions, print_marginal_probabilities
+from generator import generate_murder
+
+
+INCOMPLETE_MURDER_DATA = {
+    'Victim Sex': '?',
+    'Victim Age': '20',
+    'Victim Race': '?',
+    'Perpetrator Sex': 'male',
+    'Perpetrator Age': '?',
+    'Perpetrator Race': 'asian',
+    'Relationship': 'friend',
+    'Weapon': 'strangulation'
+}
 
 
 def main():
@@ -14,6 +27,7 @@ def main():
     parser.add_argument('--print_distributions', action='store_true')
     parser.add_argument('--print_dependencies', action='store_true')
     parser.add_argument('--print_marginal_probabilities', action='store_true')
+    parser.add_argument('--generate', action='store_true')
     args = parser.parse_args()
     
     data = get_prepared_data()
@@ -31,9 +45,11 @@ def main():
         print_feature_dependencies(dependencies_dict)
     if args.print_marginal_probabilities:
         print_marginal_probabilities(marginal_probabilities_dict)
-
     if args.additional_plots:
         plot_probability_distributions(marginal_probabilities_dict)
+    if args.complete:
+        generated_murder = generate_murder(dependencies_dict, marginal_probabilities_dict, INCOMPLETE_MURDER_DATA)
+        print(generated_murder)
 
 
 if __name__ == '__main__':
