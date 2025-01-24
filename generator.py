03c2bb251c1835c9b1787c1c7cab0d7174b39c3c
Pedziwiatrr <michalozaurus@gmail.com>
2025-01-24 15:19:08 +0100

    Finally implemented semi-working generator but it still has problems when there are '?' parents

--- generator.py
+++ generator.py
@@ -1,25 +1,44 @@
 import random
 import numpy as np
 
+def generate_murder(distributions_dict, marginal_probabilities_dict, murder_data):
+    for feature, value in murder_data.items():
+        if value == '?':
+            if feature in distributions_dict:
+                print(f"\nGENERATING FEATURE: {feature}")
+                # Use the conditional probability distribution
+                distribution = distributions_dict[feature]
 
-def generate_murder(probability_distribution, marginal_probabilities, murder):
+                # Find the probabilities for the feature based on known parents (feature's influencers)
+                feature_index = distribution.variables.index(feature)
+                parent_values = {parent: murder_data[parent] for parent in distribution.variables if parent != feature}
+                print(f"parent_values: {parent_values}")
 
-    missing_features = [feature for feature in probability_distribution.keys()
-                        if murder[feature] == '?']
+                # Get all values probabilities
+                possible_values = distribution.state_names[feature]
+                probabilities = distribution.values
+                print(f"Type of probabilities: {type(probabilities)}")
+                print(f"Shape of probabilities: {probabilities.shape}")
 
-    for feature in missing_features:
-        cpd = probability_distribution[feature]
+                if probabilities.ndim > 1:
+                    # Iterating in reverse so the previous indexes won't change
+                    for parent, parent_value in reversed(list(parent_values.items())):
+                        if parent_value != '?':
+                            parent_index = distribution.variables.index(parent)
+                            state_index = distribution.state_names[parent].index(parent_value)
+                            probabilities = np.take(probabilities, state_index, axis=parent_index)
+                            print(f"parent_index: {parent_index}, parent_value: {parent_value}, state_index: {state_index}, probabilities dim: {probabilities.ndim}")
 
+                combined_probabilities = probabilities / np.sum(probabilities)
+                print(f"Normalized probabilities: {combined_probabilities}")
 
-        if len(cpd.variables) == 1:
-        # len == 1 -> no dependencies, it is a marginal probability
-            probabilities = marginal_probabilities[feature]
-            # choose the value basing on stated probabilities
-            value = random.choices(list(probabilities.keys()), weights=list(probabilities.values()))[0]
-        else:
-        # len > 1 -> this feature depends on others
-            pass
+                # Roulette selection like in evolution algorithm
+                murder_data[feature] = random.choices(possible_values, weights=combined_probabilities, k=1)[0]
+            else:
+                probabilities = marginal_probabilities_dict[feature]
+                possible_values = list(probabilities.keys())
+                weights = list(probabilities.values())
 
-        murder[feature] = value
+                murder_data[feature] = random.choices(possible_values, weights=weights, k=1)[0]
 
-    return murder
+    return murder_data
