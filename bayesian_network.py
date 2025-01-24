071a28435686f5bc040d427fd5818f7b3c9ed2bd
Pedziwiatrr <michalozaurus@gmail.com>
2025-01-24 02:36:36 +0100

    implemented feature's dependencies and probabilities distribution calculation

--- bayesian_network.py
+++ bayesian_network.py
@@ -1,5 +1,6 @@
 from pgmpy.models import BayesianNetwork
 from pgmpy.estimators import HillClimbSearch, BicScore, MaximumLikelihoodEstimator
+from collections import defaultdict
 
 
 def create_network(data):
@@ -14,3 +15,21 @@ def create_network(data):
     network.fit(data, estimator=MaximumLikelihoodEstimator)
 
     return network
+
+
+def get_feature_probability_distribution(network):
+    probability_distribution = {}
+    for feature in network.nodes():
+        distribution = network.get_cpds(feature)
+        probability_distribution[feature] = distribution
+
+    return probability_distribution
+
+
+def get_feature_dependencies(network):
+    dependencies = defaultdict(list)
+    for edge in network.edges():
+        dependencies[edge[0]].append((edge[1]))
+        #print(f"edge: {edge[0]} -> {edge[1]}")
+    #print(f"\ndependencies: {dependencies}\n")
+    return dependencies
