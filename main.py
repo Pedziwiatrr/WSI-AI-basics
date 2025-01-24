ffdf7d4dad8729da3d15eaac9739db1ce72a918d
Pedziwiatrr <michalozaurus@gmail.com>
2025-01-24 02:42:07 +0100

    implemented basic argparser

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ from visualizer import visualize_network, print_feature_dependencies, print_prob
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--network_plot', action='store_true')
+    parser.add_argument('--print', action='store_true')
+    args = parser.parse_args()
+
     data = get_numerical_data()
     network = create_network(data)
-    visualize_network(network)
+    if args.network_plot:
+        visualize_network(network)
 
     dependencies_dict = get_feature_dependencies(network)
-    print_feature_dependencies(dependencies_dict)
 
     distributions_dict = get_feature_probability_distribution(network)
-    print_probabilities_distribution(distributions_dict)
+
+    if args.additional_prints:
+        #print_probabilities_distribution(distributions_dict)
+        #print_feature_dependencies(dependencies_dict)
 
 
 if __name__ == '__main__':
